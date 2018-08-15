ef7a7c01c09484ba9c46dc9ba41d94bad5dfc2f7
Hannah <hrglasser94@gmail.com>
2018-08-15 11:54:53 -0400

    Adjusted my functions to prepare for changing to a menu object in my runPhonebook function.

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ var fs = require('fs');
 var filename = 'phonebook.txt';
 var phonebook = {};
 
+var interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 var readFile = function(filename) {
     fs.readFile(filename, function(err, contents) {
        if (!err) {
@@ -17,22 +22,6 @@ var readFile = function(filename) {
    })
 }
 
-var writeFile = function(filename, phonebook) {
-    var writePhonebook = JSON.stringify(phonebook);
-    fs.writeFile(filename, writePhonebook, 'utf8', function(err) {
-       if (!err) {
-            console.log("Made changes to the phonebook!");
-       } else {
-           console.log(err);
-       }
-   })
-}
-
-var interface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 var create = function(phonebook, filename) {
     interface.question('What do you want to do (1-5)?' + '\n', (function(answer) {
         runPhonebook(answer, phonebook, filename);
@@ -40,7 +29,7 @@ var create = function(phonebook, filename) {
 }
 
 var printHeader = function (phonebook, filename) {
-    var choiceArray = 
+    var menuArray = 
     ['Electronic Phone Book',
         '=====================',
         '1. Look up an entry',
@@ -48,7 +37,7 @@ var printHeader = function (phonebook, filename) {
         '3. Delete an entry',
         '4. List all entries',
         '5. Quit'];
-    console.log(choiceArray.join('\n'));
+    console.log(menuArray.join('\n'));
     create(phonebook, filename);
 };
 
@@ -64,7 +53,7 @@ var lookupEntry = function(phonebook, filename) {
     })
 }
 
-var setEntry = function(filename, phonebook) {
+var setEntry = function(phonebook, filename) {
     interface.question('Name: ', function(name) {
         interface.question('Phone Number: ', function(number) {
             phonebook[name] = number;
@@ -74,7 +63,7 @@ var setEntry = function(filename, phonebook) {
     })
 }
 
-var deleteEntry = function(filename, phonebook) {
+var deleteEntry = function(phonebook, filename) {
     interface.question('Name: ', function(name) {
         if (phonebook[name]) {
             delete phonebook[name];
@@ -87,24 +76,42 @@ var deleteEntry = function(filename, phonebook) {
     })
 }
 
-var listAllEntries = function(phonebook) {
+var listAllEntries = function(phonebook, filename) {
     console.log(phonebook);
+    printHeader(phonebook, filename);
 }
 
+var writeFile = function(phonebook, filename) {
+    var writePhonebook = JSON.stringify(phonebook);
+    fs.writeFile(filename, writePhonebook, 'utf8', function(err) {
+       if (!err) {
+            console.log("Made changes to the phonebook!\nGoodbye :)");
+            interface.close();
+       } else {
+           console.log(err);
+       }
+   })
+}
+
+//Get rid of if/else statements and replace with a menu object
 var runPhonebook = function(answer, phonebook, filename) {
+    // var menu = {
+    //     "1": lookupEntry(phonebook, filename), 
+    //     "2": setEntry(phonebook, filename),
+    //     "3": deleteEntry(phonebook, filename),
+    //     "4": listAllEntries(phonebook, filename),
+    //     "5": writeFile(phonebook, filename),
+    // }
     if (answer === '1') {
         lookupEntry(phonebook, filename);
     } else if (answer === '2') {
-        setEntry(filename, phonebook);
+        setEntry(phonebook, filename);
     } else if (answer === '3') {
-        deleteEntry(filename, phonebook);
+        deleteEntry(phonebook, filename);
     } else if (answer === '4') {
-        listAllEntries(phonebook);
-        printHeader(phonebook, filename);
+        listAllEntries(phonebook, filename);
     } else if (answer === '5') {
-        writeFile(filename, phonebook);
-        console.log('Goodbye!');
-        interface.close();
+        writeFile(phonebook, filename);
     } else {
         console.log("Uh-oh! You did not enter a valid number.")
         printHeader(phonebook, filename);
