cb0ddbffac16c424b8a6595517ee24dbf12c91c4
Hannah <hrglasser94@gmail.com>
2018-08-20 15:08:46 -0400

    Added Jonathan's front-end files. Updated main.js so that all of the route paths are now functional

--- main.js
+++ main.js
@@ -3,52 +3,89 @@ const http = require('http');
 
 let generateRandom = () => Math.floor(Math.random() * 10000000 + 1);
 
-let getPhonebook = (req, res, data, contact) => {
-    res.end(data);
+let getPhonebook = (req, res, data, matches) => {
+    res.end(JSON.stringify(data));
 }
 
-// let readBody = (req, callback) => {
-//     let body="";
-//     req.on('data', (chunk) => {
-//         body += chunk.toString();
-//     });
-//     req.on('end', () => {
-//         callback(body);
-//     })
-// }
+let getContact = (req, res, data, matches) => {
+    let id = matches[0];
+    res.end(JSON.stringify(data[id]));
+}
+
+let deleteContact = (req, res, data, matches) => {
+    let id = matches[0];
+    delete data[id];
+    fs.writeFile('phonebook.txt', JSON.stringify(data), 'utf8', (err) => {
+        res.end(`Contact has been deleted.`);
+    })
+}
+
+let readBody = (req, callback) => {
+    let body="";
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        callback(body);
+    })
+}
+
+let addContact = (req, res, data, matches) => {
+    readBody(req, (body) => {
+        let newContact = JSON.parse(body);
+        let id = generateRandom();
+        newContact.id = id;
+        data[id] = newContact;
+        fs.writeFile('phonebook.txt', JSON.stringify(data), 'utf8', (err) => {
+            res.end(`New contact has been created.`); 
+        })
+    })
+}
+
+let updateContact = (req, res, data, matches) => {
+    readBody(req, (body) => {
+        let currentContact = matches[0];
+        let contactUpdates = JSON.parse(body);
+        data[currentContact] = contactUpdates;
+        fs.writeFile('phonebook.txt', JSON.stringify(data), 'utf8', (err) => {
+            res.end(`${(JSON.stringify(data[currentContact]["name"]))} has been updated.`); 
+        })
+    })
+}
+
 let notFound = (req, res) => {
     res.end('404 Not Found');
 }
 
 let routes = [
-    // {
-    //     method: 'GET',
-    //     url: '/phonebook/',
-    //     run: getContact
-    // },
-    // {
-    //     method: 'DELETE',
-    //     url: '/phonebook/',
-    //     run: deleteContact
-    // },
-    // {
-    //     method: 'PUT',
-    //     url: '/phonebook/',
-    //     run: updateContact
-    // },
     {
         method: 'GET',
-        url: '/phonebook',
+        url: /^\/phonebook\/([0-9]+)$/,
+        run: getContact
+    },
+    {
+        method: 'DELETE',
+        url: /^\/phonebook\/([0-9]+)$/,
+        run: deleteContact
+    },
+    {
+        method: 'PUT',
+        url: /^\/phonebook\/([0-9]+)$/,
+        run: updateContact
+    },
+    {
+        method: 'GET',
+        url: /^\/phonebook\/?$/,
         run: getPhonebook
     },
-    // {
-    //     method: 'POST',
-    //     url: '/phonebook',
-    //     run: addContact
-    // },
+    {
+        method: 'POST',
+        url: /^\/phonebook\/?$/,
+        run: addContact
+    },
     {
         method: 'GET',
-        url: '',
+        url: /^.*$/,
         run: notFound
     }
 ]
@@ -56,52 +93,15 @@ let routes = [
 //Everything inside of this function will be run anytime someone connects to your server
 let server = http.createServer((req, res) => {
     fs.readFile('phonebook.txt', 'utf8', (err, data) => {
-        let contacts = JSON.parse(data);
-        let urlArray = req.url.split('/');
-        let contact = urlArray.pop();
-        for (var route of routes) {
-            if (route.url.startsWith(route.url) && route.method === req.method) {
-                route.run(req, res, data, contact);
-                return true;
+        for (route of routes) {
+            if (route.url.test(req.url) && route.method === req.method) {
+                let matches = route.url.exec(req.url);
+                let contacts = JSON.parse(data);
+                route.run(req, res, contacts, matches.slice(1));
+                break
             }
         }
     })
-    //     } else if (req.url.startsWith('/phonebook/') && req.method === "GET") {
-    //         if (contacts[contact]) {
-    //             res.end(JSON.stringify(contacts[contact]));
-    //         } else {
-    //             res.end('No contact with that name.');
-    //         }
-    //     } else if (req.url.startsWith('/phonebook/') && req.method === "DELETE") {
-    //         delete contacts[contact];
-    //         fs.writeFile('phonebook.txt', JSON.stringify(contacts), 'utf8', (err) => {
-    //             if (!err) {
-    //                 res.end(`${contact} has been deleted.`);
-    //             } else {
-    //                 res.end("No contact with that name");
-    //             }
-    //         })
-    //     } else if (req.url.startsWith('/phonebook') && req.method === "POST") {
-    //         readBody(req, (body) => {
-    //             let newContact = JSON.parse(body);
-    //             let id = generateRandom();
-    //             newContact.id = id;
-    //             contacts[id] = newContact;
-    //             fs.writeFile('phonebook.txt', JSON.stringify(contacts), 'utf8', (err) => {
-    //                 res.end(`${JSON.stringify(contacts[newContact])} has been created.`); 
-    //             })
-    //         })
-    //     } else if (req.url.startsWith('/phonebook/') && req.method === "PUT") {
-    //         readBody(req, (body) => {
-    //             let currentContact = contact;
-    //             let contactUpdates = JSON.parse(body);
-    //             contacts[currentContact] = contactUpdates;
-    //             fs.writeFile('phonebook.txt', JSON.stringify(contacts), 'utf8', (err) => {
-    //                 res.end(`${(JSON.stringify(contacts[currentContact]["name"]))} has been updated.`); 
-    //             })
-    //         })
-    //     } 
-    // })
 })
 
 server.listen(3102);
